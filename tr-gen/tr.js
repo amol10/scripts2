4ea2be8fbe388ef71f1c5eb0a3583572826ba6ba
Amol <babaiscool@gmail.com>
2025-07-19 17:57:35 +0530

    Added the faeture of selecting the table style, either the included one, or the external CSS lib.

--- tr-gen/tr.js
+++ tr-gen/tr.js
@@ -5,6 +5,15 @@ function debug(s) { console.log(s); }
 //debug(argv);
 var row_count = argv[2];
 var col_count = argv[3];
+var css_library = argv[4];
+
+css_library_url = "";
+css_class = "";
+switch (css_library){
+    case 'b': css_library_url = "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr\" crossorigin=\"anonymous\">";
+        css_class = "table";
+    //default: 
+}
 
 var row_count_i = parseInt(row_count);
 var col_count_i = parseInt(col_count);
@@ -14,17 +23,28 @@ var col_count_i = parseInt(col_count);
 
 var output = "";
 
+style = `
+    <style>
+		table, tr, td {
+			border: 1px solid black;
+		}
+	</style>
+`
 output += `<html>
 	<head>
-		<style>
-			table, tr, td {
-				border: 1px solid black;
-			}
-		</style>
+`
+
+if (css_library_url != "") {
+    output += css_library_url;
+} else {
+    output += style;
+}
+
+output += `
 	</head>
 	<body>
 `
-output += "<table>";
+output += "<table" + ' class=\"' + css_class + '\">';
 for (let i = 0; i < row_count_i; i++) {
     output += "\t<tr>";
     for (let j = 0; j < col_count_i; j++) {
