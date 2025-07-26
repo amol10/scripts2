5942aafbb414f57da9278c03b03bae2b2c1829e3
Amol <babaiscool@gmail.com>
2025-07-26 22:25:45 +0530

    In progress, adding dictionary to track the dir structure.

--- posts-mgr/mgr.js
+++ posts-mgr/mgr.js
@@ -11,16 +11,25 @@ const path = require('path');
 
 console.log("loaded");
 
+var skip_h = true;
+
 //var fs = new FileSystem();
+var d = {}
 
-async function walk_dir(dir_path) {
+async function walk_dir(dir_path, level = " ") {
     const files = await fs.readdir(dir_path, { withFileTypes: true });
-
+    d{dir_path.parse()}- {}
     await Promise.all(
         files.map(file => {
             const full_path = path.join(dir_path, file.name);
             if (file.isDirectory()) {
-                console.log(file.name);
+                if (skip_h && file.name[0] == '.') {
+                    console.log(level + "skipping, hidden " + file.name);
+                } else {
+                    console.log(level + file.name);
+                    d{file.name} = {};
+                    walk_dir(path.join(full_path), level + " ");
+                }
             }
         })
     );
