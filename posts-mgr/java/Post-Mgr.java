faf4b4fd0511531302a4b6e9f9849a170d4f55b3
Amol <babaiscool@gmail.com>
2025-08-06 19:26:56 +0530

    Added some more code.

--- posts-mgr/java/Post-Mgr.java
+++ posts-mgr/java/Post-Mgr.java
@@ -27,9 +27,9 @@ public class Main {
 
         //Stream<File> d3 = Arrays.stream(d2); //.map(s -> File(p, s));
 
-        File[] d3 = new File[d2.length];
+        String[] d3 = new String[d2.length];
         for (int di = 0; di < d2.length; di += 1) {
-            d3[di] = new File(p, d2[di]);
+            d3[di] = new String(p + File.separator + d2[di]);
         }
 
         for (int i = 0; i < d3.length; i += 1) {
@@ -37,7 +37,16 @@ public class Main {
             System.out.println(d3[i]); 
         }
 
+        String[] m = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
-
+        String[] m2 = new String[m.length * d3.length];
+        Integer mc = 0;
+        for (String di2: d3) {
+                for (String mi: m) {
+                    m2[mc] = new String(di2 + File.separator + mi);
+                    System.out.println(m2[mc]);
+                    mc += 1;
+                }
+        }
     }
 }
