47718a2dd7c8ff30ecf081f77572bec8da888f0a
Amol <babaiscool@gmail.com>
2025-08-14 21:30:02 +0530

    Added some code.

--- steam-rec/Steam-Rec.ps1
+++ steam-rec/Steam-Rec.ps1
@@ -6,16 +6,20 @@ $p3 = "C:\Users\amole\Videos\"
 $p2e = Test-Path -Path $p2
 $p2e
 if ($p2e -eq $False) {
-    Write-Host Dir does not exist.
+    Write-Host "Dir does not exist."
     New-Item -Name Steam-Rec -Path $p3 -ItemType Directory
 }
 $p2e = Test-Path -Path $p2
 $p2e
 if ($p2e -eq $True) {
-    Write-Host Dir created.
+    Write-Host "Dir created."
 } else {
     exit 0
 }
 
 $ff = "C:\Users\amole\Bin\ffmpeg\bin\ffmpeg.exe"
-$d | Select-Object -Property
+#$d | Select-Object -Property
+
+$d2 = $d | ForEach { Get-ChildItem $p\$_ } | ForEach { $_.FullName }
+Write-Host $d2.Length "files found."
+
